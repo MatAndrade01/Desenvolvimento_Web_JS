6db8afc3db9d19d36cfe34b831ba73b2028072dd
MatAndrade01 <ltbmatheus141012@gmail.com>
2024-12-11 12:39:34 -0300

    Aprendendo a mexer com styles e eventos pelo JS

---
+++ aula-20/js/index.js
@@ -0,0 +1,57 @@
+const buttonAumentar = document.querySelector("#aumentar-botao");
+const buttonDiminuir = document.querySelector("#diminuir-botao");
+
+const contadorElemento = document.querySelector("#contador");
+
+const input = document.querySelector("#input");
+
+buttonAumentar.addEventListener('click', (event) => {
+    const valorAtual = Number(contadorElemento.textContent);
+
+    contadorElemento.textContent = valorAtual + 1;
+
+    buttonAumentar.classList.add("btn");
+    buttonDiminuir.classList.remove("btn");
+});
+
+buttonDiminuir.addEventListener('click', (event) => {
+    const valorAtual = Number(contadorElemento.textContent);
+
+    contadorElemento.textContent = valorAtual - 1;
+    buttonDiminuir.classList.add("btn");
+    buttonAumentar.classList.remove("btn");
+});
+
+input.addEventListener('input', (event) => {
+    console.log(input.value);
+});
+
+// Adicionando estilos inline no elemento contador
+
+contadorElemento.style.color = "red";
+contadorElemento.style.padding = "0 2rem";
+contadorElemento.style.border = "1px solid #AAA";
+contadorElemento.style.width = "150px";
+
+// Manipular Classes
+
+console.log(buttonDiminuir.classList);
+
+const themeButton = document.querySelector("#theme");
+
+let darkTheme = false; 
+
+themeButton.addEventListener('click', () => {
+
+    darkTheme = !darkTheme;
+
+    const body =  document.querySelector('body');
+
+    if (darkTheme) {
+        body.style.background = "black";
+        body.style.color = "white";
+    } else {
+        body.style.background = "white";
+        body.style.color = "black";
+    }
+})
